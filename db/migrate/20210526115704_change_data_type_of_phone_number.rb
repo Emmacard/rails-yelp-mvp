db07a5508c659664aa28d80362b69304064c21c6
Emma Cardon <emma.cardon7@gmail.com>
2021-05-26 18:31:13 +0100

    challenge2 progress

---
+++ db/migrate/20210526115704_change_data_type_of_phone_number.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeOfPhoneNumber < ActiveRecord::Migration[6.0]
+  def change
+    change_column :restaurants, :phone_number, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  puts 'creating a restaurant'
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+  puts "finished making #{restaurant.id}"
+end
+
